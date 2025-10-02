5e1fd21de331d5dd85e8815dd7231002ce921d8e
Salmankhan20032 <sk03961233@gmail.com>
2025-10-02 14:14:16 +0300

    Update intenrshi

--- internships/serializers.py
+++ internships/serializers.py
@@ -2,40 +2,72 @@
 
 from rest_framework import serializers
 from .models import Internship, Task, UserInternship, Submission
+from cloudinary.models import CloudinaryField
+from cloudinary.utils import cloudinary_url
+
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'order']
+        fields = ["id", "title", "description", "order"]
+
 
 class InternshipListSerializer(serializers.ModelSerializer):
+    thumbnail = serializers.SerializerMethodField()
+
     class Meta:
         model = Internship
-        fields = ['id', 'title', 'thumbnail', 'field', 'length_days', 'created_at']
+        fields = ["id", "title", "thumbnail", "field", "length_days", "created_at"]
+
+    def get_thumbnail(self, obj):
+        if obj.thumbnail:
+            url, options = cloudinary_url(obj.thumbnail.public_id)
+            return url
+        return None
+
 
 class InternshipDetailSerializer(serializers.ModelSerializer):
     tasks = TaskSerializer(many=True, read_only=True)
 
     class Meta:
         model = Internship
-        fields = ['id', 'title', 'thumbnail', 'description', 'field', 'length_days', 'created_at', 'tasks']
+        fields = [
+            "id",
+            "title",
+            "thumbnail",
+            "description",
+            "field",
+            "length_days",
+            "created_at",
+            "tasks",
+        ]
+
 
 class SubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submission
         fields = [
-            'project_link', 'fully_completed', 'experience_feedback', 
-            'difficulty_rating'
+            "project_link",
+            "fully_completed",
+            "experience_feedback",
+            "difficulty_rating",
         ]
+
 
 class UserInternshipSerializer(serializers.ModelSerializer):
     internship = InternshipListSerializer(read_only=True)
     submission = SubmissionSerializer(read_only=True)
-    
+
     class Meta:
         model = UserInternship
         # --- THE 'is_started' FIELD IS NOW INCLUDED HERE ---
         fields = [
-            'id', 'internship', 'enrollment_date', 'status', 
-            'is_started', 'intro_completed', 'roadmap_completed', 'submission'
+            "id",
+            "internship",
+            "enrollment_date",
+            "status",
+            "is_started",
+            "intro_completed",
+            "roadmap_completed",
+            "submission",
         ]
